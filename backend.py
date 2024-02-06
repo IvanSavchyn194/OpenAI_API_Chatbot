a637ac1f0a7615a138de63723bb2ae98c56e9ed6
ivans <ivans@varyence.com>
2024-02-06 22:11:40 +0200

    Create ChatBot class and get_response method

--- backend.py
+++ backend.py
@@ -1,2 +1,22 @@
+import openai
+import os
+
+
 class ChatBot:
-    pass
+    def __init__(self):
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    def get_response(self, user_input):
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+                {"role": "user", "content": user_input}
+            ]
+        ).choices[0].message.content
+        return response
+
+
+if __name__ == "__main__":
+    chatbot = ChatBot()
+    response = chatbot.get_response("Write a joke about birds.")
+    print(response)
